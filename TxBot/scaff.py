104076b52c6ddf9afc61913820868d6e11b6c190
jawahar273 <jawahar273@gmail.com>
2019-04-24 14:47:40 +0530

    imporved version of scaff.py for generating file in response

--- TxBot/scaff.py
+++ TxBot/scaff.py
@@ -1,4 +1,5 @@
 import os
+from utils import invert_title
 
 '''
 Scaff module helpful in increating the prodicitive of
@@ -18,10 +19,19 @@ before training.
 
 '''
 
-def gen_response(base_path = "TxBot_response"):
 
-    input_path = input(f'name of the file and path from `{base_path}`: ')
-    scaff_path = input_path
+def exit_now():
+    print('good bye..')
+    exit(0)
+
+
+def gen_response(base_path="TxBot_response"):
+
+    print('*' * 20, 'Generate Response Files Easy', '*' * 20)
+    print(f'The base folder is from `{base_path}`')
+    sub_path = input(f'name of the sub folder such as for Example `_task`:')
+    input_path = input(f'name of the intent file for response: ')
+    file_name = invert_title(input_path)
     input_path = f'_{input_path}'
 
     try:
@@ -29,38 +39,60 @@ def gen_response(base_path = "TxBot_response"):
         if (os.path.isdir(input_path)):
             input_path = os.path.split(input_path)[0]
 
-        input_path = os.path.join(os.path.dirname(__file__), base_path, input_path)
-        os.mkdir(input_path)
+        input_path = os.path.join(os.path.dirname(__file__),
+                                  base_path, sub_path, input_path)
+        os.makedirs(input_path)
 
     except FileExistsError as e:
         pass
 
-    scaff_path = os.path.join(input_path, scaff_path)
+    scaff_path = os.path.join(input_path, file_name)
+    print('abs folder', scaff_path)
+    print('If somthing is wrong, press 1  to exit or press any key')
+    if input('..') == '1':
+        exit_now()
 
     with open(f'{scaff_path}.html', 'w') as file:
-
-        file.close()
+        pass
 
     with open(f'{scaff_path}.txt', 'w') as file:
-
-        file.close()
+        pass
 
     with open(f'{scaff_path}.py', 'w') as file:
+        class_template = f'''
+from TxBot_response.abstract_response import TxBaseResponse
 
-        file.close()
+class {file_name.title()}(TxBaseResponse):
+
+    def __init__(self):
+        super(GetListOfTask, self).__init__(self)
+
+    def get_class_name(self):
+        return self.__class__.__name__
+
+    def render(self):
+        pass
+
+        '''
+        file.write(class_template)
 
 
 print_text = '''
-1. help
+0. Exit
+1. Help
 2. Generate response
 '''
 
-user_number = int(input('command: '))
 print('press 1 for help.')
-if user_number == 1:
-    print(print_text)
-elif user_number == 2:
-    gen_response()
+from sys import exit
+while True:
 
+    user_number = int(input('command: '))
+    if user_number == 0:
+        exit_now()
 
+    elif user_number == 1:
+        print(print_text)
 
+    elif user_number == 2:
+        gen_response()
