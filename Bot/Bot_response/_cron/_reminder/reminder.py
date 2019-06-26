22d6e5ce9e1b6f36277e48dd3731db2d428a2967
jawahar s <jawahar@bytize.in>
2019-06-27 01:13:26 +0530

    reminder basic level done

--- Bot/Bot_response/_cron/_reminder/reminder.py
+++ Bot/Bot_response/_cron/_reminder/reminder.py
@@ -1,9 +1,10 @@
-from os import getenv
+
 
 from plyer import notification
 
 from Bot.Bot_response.abstract_response import BaseResponse
 from config.stage import db
+from .scheduler import main, list_of_all_task
 
 
 class Reminder(BaseResponse):
@@ -19,6 +20,8 @@ class Reminder(BaseResponse):
     def render(self, pretty=None):
 
         self.class_name = self.get_class_name()  # class name
+        _list_of_task = None
+        wait_for_countdown = None
 
         super(Reminder, self).render(
             class_name=self.class_name, sub_path='_cron'
@@ -33,14 +36,17 @@ class Reminder(BaseResponse):
         when = self.get_slot_by_name('when')
         message = self.get_slot_by_name('message')
         intended_time = self.get_slot_by_name('intendedTime')
-        # huey https://huey.readthedocs.io/en/latest/guide.html
 
         print('countdown:', countdown, ',mes:', message, ',inttime:', self.get_slot_by_name('intendedTime'), ',when:', when,)
+        print(self.get_slot_by_name('listOfCountDown'))
         if countdown and not when and not intended_time or countdown and intended_time:
             # start the count down right now.
 
             # passing to schedular
-            from .scheduler import main
-            main(countdown)
+            main(countdown, message)
+            wait_for_countdown = True
 
-        return self.render_template.render(pretty=pretty)
+        elif self.get_slot_by_name('listOfCountDown'):
+            _list_of_task = list_of_all_task()  
+
+        return self.render_template.render(pretty=pretty, list_of_task=_list_of_task, wait_for_countdown=wait_for_countdown)
--- Bot/Bot_response/_cron/_reminder/scheduler.py
+++ Bot/Bot_response/_cron/_reminder/scheduler.py
@@ -1,9 +1,10 @@
 
 from time import sleep, strftime
-
+from os import getenv
 from plyer import notification
 
 from config.stage import Huey
+from .models import CountdownTracker
 
 
 def to_secs(mins):
@@ -15,21 +16,42 @@ def to_mins(hrs):
     return float(hrs) * 60
 
 
-@Huey.task()
-def start_count_down_basic_case(_time):
+@Huey.task(context=True)
+def start_count_down_basic_case(_time, message, task=None):
+    """
+    task will and created and deleted after finishing
+    the task can still be revoked it the task is running
+    """
     # handling basic case for count down and imidetely
     countdown_sec = None
-    notification.notify(f'starting count down')
+
+    if (getenv('TARGET_PLATFORM') != 'web'):
+
+        notification.notify(f'starting count down')
+
     if _time.find('min') is not -1:
+
         countdown_sec = to_secs(_time.split()[0])
+
     elif (_time.find('hr') or _time.find('hour')) is not -1:
+
         countdown_sec = to_secs(
             to_mins(_time.split()[0]) 
         )
+
     elif _time.find('sec') is not -1:
+
         countdown_sec = int(_time.split()[0])
+
     else:
         return 
+    # creating task id in redis
+    CountdownTracker.create(
+        id=task.id,
+        seconds=countdown_sec,
+
+    )
+    
     interval = {
         'half_interval': countdown_sec // 2,
         'quator_interval': countdown_sec // 4
@@ -37,13 +59,47 @@ def start_count_down_basic_case(_time):
     while countdown_sec >= 1:
         countdown_sec -= 1
         if interval['half_interval'] == countdown_sec:
-            notification.notify(f'half of the time has been over for {_time}')
+
+            if (getenv('TARGET_PLATFORM') != 'web'):
+                notification.notify(f'half of the time has been over for {_time}')
+
         elif interval['quator_interval' ] == countdown_sec:
-            notification.notify(f'quator of the time has been over for {_time}')
+
+            if (getenv('TARGET_PLATFORM') != 'web'):
+                notification.notify(f'quator of the time has been over for {_time}')
 
         sleep(1)
-    notification.notify(f'count down done')         
+    if (getenv('TARGET_PLATFORM') != 'web'):
+        notification.notify(f'Message: {message}')
+
+    CountdownTracker.delete(id=task.id)
 
 
-def main(time):
-    start_count_down_basic_case(time)
+def main(time, message):
+
+    start_count_down_basic_case(time, message)
+
+
+def list_of_all_task():
+
+    return [
+        {
+            'id': task.id,
+            'desc': task.desc,
+            'seconds': task.seconds
+        }
+        for task in CountdownTracker.all()
+    ]
+
+
+def revoke_task_by_id(_id):
+    
+    try:
+
+        result = CountdownTracker.get(id=_id)
+        Huey.revoke_by_id(result)
+
+    except ValueError:
+
+        if (getenv('TARGET_PLATFORM') != 'web'):
+            notification.notify(f'No id found')         
